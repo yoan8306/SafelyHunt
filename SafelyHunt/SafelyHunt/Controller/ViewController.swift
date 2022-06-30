32a62e7beb71a9b05a18c5d99fe59cc380403a5d
yoan8306 <yoan8306@wanadoo.fr>
2022-06-30 10:02:33 +0200

    Initial commit

---
+++ SafelyHunt/SafelyHunt/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SafelyHunt
+//
+//  Created by Yoan on 28/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

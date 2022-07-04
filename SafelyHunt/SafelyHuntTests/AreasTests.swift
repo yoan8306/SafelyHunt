6869d715ae5042ec0b6ed065dc3a56fa6a2d0865
yoan8306 <yoan8306@wanadoo.fr>
2022-07-04 12:52:20 +0200

    firebase connected and test

---
+++ SafelyHunt/SafelyHuntTests/AreasTests.swift
@@ -0,0 +1,16 @@
+//
+//  AreasTests.swift
+//  SafelyHuntTests
+//
+//  Created by Yoan on 01/07/2022.
+//
+
+import XCTest
+@testable import SafelyHunt
+
+class AreasTests: XCTestCase {
+    func testGivenNoAreaInDataBase_WhenAddArea_ThenDataBaseGetOneArea() {
+        let area = true
+        XCTAssertTrue(area)
+    }
+}

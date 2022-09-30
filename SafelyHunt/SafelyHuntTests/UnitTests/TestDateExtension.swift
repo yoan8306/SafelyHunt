0d442d9aa50590d1f64d53e3cc694d9d7ed132bd
yoan8306 <yoan8306@wanadoo.fr>
2022-09-30 22:40:24 +0200

    Continue test interface

--- SafelyHunt/SafelyHuntTests/UnitTests/TestDateExtension.swift
+++ SafelyHunt/SafelyHuntTests/UnitTests/TestDateExtension.swift
@@ -11,8 +11,10 @@ import XCTest
 class TestDateExtension: XCTestCase {
 
     func testGivenDateFormatDateWhenTransformToTimeStampThenTheDateIsFormatTimeStamp() {
+        let dateFormatter = DateFormatter()
         let myDate = Date(timeIntervalSince1970: 1664352449)
-
+        dateFormatter.timeZone = .current
+       
         XCTAssertTrue(myDate.dateToTimeStamp() == 1664352449)
         XCTAssertEqual(myDate.getTime(), "10:07:29 AM")
         XCTAssertEqual(myDate.relativeDate(relativeTo: Date(timeIntervalSince1970: 1664352500)), "il y a 51 secondes")
--- SafelyHunt/SafelyHuntUITests/SafelyHuntUITests.swift
+++ SafelyHunt/SafelyHuntUITests/SafelyHuntUITests.swift
@@ -21,13 +21,36 @@ let app = XCUIApplication()
         if app.buttons["Dissmiss"].exists {
             app.buttons["Dissmiss"].tap()
         }
-
-//        let app = XCUIApplication()
         let tablesQuery = app.tables
         tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Select your area hunt"]/*[[".cells.staticTexts[\"Select your area hunt\"]",".staticTexts[\"Select your area hunt\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         let appleAreaCellsQuery = tablesQuery/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier: "Apple area")/*[[".cells.containing(.staticText, identifier:\"Cupertino\")",".cells.containing(.staticText, identifier:\"Apple area\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         appleAreaCellsQuery.element.tap()
         appleAreaCellsQuery.buttons["More Info"].tap()
+        print(app.elementType.rawValue)
+    }
+    
+    /// Draw a new area
+    func testGivenNewAreaWhenPressMapForDrawAreaThenPolygonAreCreated() {
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Select your area hunt"]/*[[".cells.staticTexts[\"Select your area hunt\"]",".staticTexts[\"Select your area hunt\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.navigationBars["Areas List"].buttons["Add"].tap()
+        app.navigationBars["Editing area"].buttons["compose"].tap()
+        
+        let vkpointfeatureMap = app/*@START_MENU_TOKEN@*/.maps.containing(.other, identifier:"Chevron").element/*[[".maps.containing(.other, identifier:\"Target\").element",".maps.containing(.other, identifier:\"Northwest YMCA\").element",".maps.containing(.other, identifier:\"Apple Infinite Loop\").element",".maps.containing(.other, identifier:\"Peninsula Bible Church Cupertino\").element",".maps.containing(.other, identifier:\"Cupertino Hotel\").element",".maps.containing(.other, identifier:\"Chevron\").element",".maps.containing(.other, identifier:\"VKPointFeature\").element"],[[[-1,6],[-1,5],[-1,4],[-1,3],[-1,2],[-1,1],[-1,0]]],[1]]@END_MENU_TOKEN@*/
+//        vkpointfeatureMap/*@START_MENU_TOKEN@*/.swipeRight()/*[[".swipeUp()",".swipeRight()"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+//        app.navigationBars["Draw area with finger"].buttons["compose"].tap()
+//        vkpointfeatureMap.swipeRight()
+//        app/*@START_MENU_TOKEN@*/.staticTexts["Cancel"]/*[[".buttons[\"Cancel\"].staticTexts[\"Cancel\"]",".staticTexts[\"Cancel\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+    }
+    
+    
+    
+    /// Draw radius Alert
+    func testGivenSliderRadiusWhenMoveToValue400ThenUserDefaultGet400() {
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["Define your radius alert"]/*[[".cells.staticTexts[\"Define your radius alert\"]",".staticTexts[\"Define your radius alert\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.sliders.element.adjust(toNormalizedSliderPosition: 0)
+        app.sliders.element.adjust(toNormalizedSliderPosition: 0.19)
+//        print(app.sliders.element.normalizedSliderPosition)
     }
     
     /// Test Start monitoring
@@ -37,14 +60,14 @@ let app = XCUIApplication()
         }
 
         let tablesQuery = app.tables
-        let selectYourAreaHuntStaticText = tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Select your area hunt"]/*[[".cells.staticTexts[\"Select your area hunt\"]",".staticTexts[\"Select your area hunt\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        selectYourAreaHuntStaticText.tap()
-
+        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["Select your area hunt"]/*[[".cells.staticTexts[\"Select your area hunt\"]",".staticTexts[\"Select your area hunt\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
         tablesQuery/*@START_MENU_TOKEN@*/.cells.containing(.staticText, identifier:"Apple area")/*[[".cells.containing(.staticText, identifier:\"Cupertino\")",".cells.containing(.staticText, identifier:\"Apple area\")"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.element.tap()
         app.navigationBars["Areas List"].buttons["Main"].tap()
         tablesQuery.buttons["Start monitoring"].tap()
+        
     }
     
+    /// Test show profil informations
     func testGivenProfilSettingWhenClickOnProfileThenSeeDisplaynameAndDistanceTotal() {
         if app.buttons["Dissmiss"].exists {
             app.buttons["Dissmiss"].tap()
@@ -53,6 +76,11 @@ let app = XCUIApplication()
         app.tables.cells.containing(.staticText, identifier:"Profile").element.tap()
         
         XCTAssertTrue(app.staticTexts["yoyo"].exists)
-                        
+        XCTAssertTrue(app.staticTexts["yoyo@wanadoo.fr"].exists)
     }
+    
+    
+ 
+    
+    
 }

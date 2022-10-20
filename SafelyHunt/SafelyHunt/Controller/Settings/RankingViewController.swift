d1b6fe655b2cffcfa09d2663a595446f6091c390
yoan8306 <yoan8306@wanadoo.fr>
2022-10-20 09:18:53 +0200

    Ranking complete

--- SafelyHunt/SafelyHunt/Controller/Settings/RankingViewController.swift
+++ SafelyHunt/SafelyHunt/Controller/Settings/RankingViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RankingViewController: UIViewController {
     var rankingHunters: [Hunter] = []
 
+    @IBOutlet weak var yourPositionLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
@@ -46,17 +48,25 @@ extension RankingViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RankingCell", for: indexPath)
         let hunter = rankingHunters[indexPath.row]
+        
+        guard let displayname = hunter.displayName, let totalDistance = hunter.totalDistance else {
+            return cell
+        }
+        
+        let stringTotalDistance = String(format: "%.2f", totalDistance / 1000)
+        if hunter.email! == FirebaseAuth.Auth.auth().currentUser?.email {
+            yourPositionLabel.text = "Your are at \(indexPath.row + 1) \(displayname) \(stringTotalDistance) km"
+        }
 
         if #available(iOS 14.0, *) {
             var content = cell.defaultContentConfiguration()
-            content.text = "\(indexPath.row) - \(String(describing: hunter.displayName!))"
-            content.secondaryText = String(describing: hunter.totalDistance!)
+            content.text = "\(indexPath.row + 1)/  \(String(describing: displayname))"
+            content.secondaryText = "\(stringTotalDistance) km"
 
             cell.contentConfiguration = content
         } else {
-            cell.textLabel?.text =  "\(indexPath.row) - \(String(describing: hunter.displayName!))"
-            cell.detailTextLabel?.text =  String(describing: hunter.totalDistance!)
-
+            cell.textLabel?.text =  "\(indexPath.row + 1) - \(String(describing: displayname))"
+            cell.detailTextLabel?.text =  "\(stringTotalDistance) km"
         }
 
         return cell
